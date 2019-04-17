8eb1da82fd35562c9c6682a4bb58d02dd0fb44e3
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2019-04-17 13:22:21 -0700

    Create create-worker-instances.sh

---
+++ scripts/create-worker-instances.sh
@@ -0,0 +1,14 @@
+for i in 0 1 2; do
+  gcloud compute instances create worker-${i} \
+    --async \
+    --boot-disk-size 200GB \
+    --can-ip-forward \
+    --image-family ubuntu-1804-lts \
+    --image-project ubuntu-os-cloud \
+    --machine-type n1-standard-1 \
+    --metadata pod-cidr=10.200.${i}.0/24 \
+    --private-network-ip 10.240.0.2${i} \
+    --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
+    --subnet kubernetes \
+    --tags kubernetes-the-hard-way,worker
+done
