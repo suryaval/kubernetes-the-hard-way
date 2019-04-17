eb431425c4b42c68456e2cf951a5e0c5bfe250c3
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2019-04-17 13:21:47 -0700

    Create create-controller-instances.sh

---
+++ scripts/create-controller-instances.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in 0 1 2; do
+  gcloud compute instances create controller-${i} \
+    --async \
+    --boot-disk-size 200GB \
+    --can-ip-forward \
+    --image-family ubuntu-1804-lts \
+    --image-project ubuntu-os-cloud \
+    --machine-type n1-standard-1 \
+    --private-network-ip 10.240.0.1${i} \
+    --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
+    --subnet kubernetes \
+    --tags kubernetes-the-hard-way,controller
+done
